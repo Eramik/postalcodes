d3ae1f4842a2ddfc60f30506c29be6555708dcbc
Denis <erami.ukr@gmail.com>
2019-04-28 16:39:12 +0300

    Added logger

---
+++ src/logger.js
@@ -0,0 +1,27 @@
+const winston = require('winston');
+const fs = require('fs');
+
+// Ensure logs directory ensists
+const logDirPath = `${__dirname}/../logs`;
+if (!fs.existsSync(logDirPath)) {
+    fs.mkdirSync(logDirPath);
+}
+
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({ filename: `${logDirPath}/error.log`, level: 'error' }),
+        new winston.transports.File({ filename: `${logDirPath}/combined.log` }),
+    ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()    
+    }));
+}
+
+logger.info('Logger initialized');
